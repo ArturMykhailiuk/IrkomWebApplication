67f35da4dd7e10b7ce118040b3bb3fae2902c442
Михайлюк Артур <mihajluk@ispro.ua>
2020-04-17 00:40:39 +0300

    Добавьте файлы проекта.

---
+++ IrkomWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using IrkomWebApplication.Models;
+
+namespace IrkomWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        MobileContext db;
+        public HomeController(MobileContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+    }
+}
